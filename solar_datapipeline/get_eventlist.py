cf10da88b8c887ec2bf7dce2f1b2b2d5927d67ff
KieranNesbitt <71833734+KieranNesbitt@users.noreply.github.com>
2025-04-15 12:03:58 +0100

    get_eventlist update
    get_eventlist now can get event list files within a given year

--- solar_datapipeline/get_eventlist.py
+++ solar_datapipeline/get_eventlist.py
@@ -1,18 +1,35 @@
 import pandas as pd
 import os
 headers = ("Date","Time","Type","Stations")
-def get_txt(url: str, save_folder: str):
+base_url: str = "https://soleil.i4ds.ch/solarradio/data/BurstLists/2010-yyyy_Monstein/"
+def get_txt(url: str, save_folder: str) -> None:
     try:
         text_file = url.split("/")[-1]
         print(f"Downloading event list: {text_file}")
-        df = pd.read_table(url, comment= "#", names= headers, sep='\t', on_bad_lines='skip').dropna()
+        df = pd.read_table(url, comment= "#", names= headers, sep='\t', on_bad_lines='skip', encoding="ISO-8859-1").dropna()
+        
         save_path = os.path.join(save_folder, text_file)
+        
         df.to_csv(save_path)
     except Exception as e:
         print(f"Error: {e}")
 
-def get_multiple_txt(urls:list, save_folder: str):
-    pass
+def get_multiple_txt(save_folder: str, years: list[int], months: list[int], base_url: str) -> None:
+    for url in generate_urls(years, months, base_url):
+        try:
+            get_txt(url, save_folder)
+        except Exception as e:
+            print(f"Error downloading {url}: {e}")
+
+def generate_urls(years: list[int], months: list[int], base_url: str):
+    for year in years:
+        for month in months:
+            yield f"{base_url}{year}/e-CALLISTO_{year}_{month:02d}.txt"
+
 save_folder: str = f"{os.getcwd()}\solar_data_folder\events_list"
-test_url: str = "https://soleil.i4ds.ch/solarradio/data/BurstLists/2010-yyyy_Monstein/2024/e-CALLISTO_2024_03.txt"
-get_txt(test_url, save_folder)
+test_url: str = "https://soleil.i4ds.ch/solarradio/data/BurstLists/2010-yyyy_Monstein/2024/e-CALLISTO_2024_10.txt"
+years: list[int] = [2024]
+months: list[int] = list(range(1,13))
+
+
+get_multiple_txt(save_folder, years, months, base_url)
